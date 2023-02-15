24da46283d0891b61f5633a9ceaca6a668cbaae8
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-02-15 12:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// your code here
+var nameInput = document.querySelector("#name");
+var yearInput = document.querySelector("#year");
+var form = document.querySelector("form");
+var heading = document.querySelector("h3");
+
+function submitFun(event){
+    event.preventDefault();
+    var headingValue = "https://localhost:8080/";
+    var name = nameInput.value;
+    var year = yearInput.value;
+    
+    if(name && year){
+        headingValue += "?name=" + name + "&year=" + year;
+    }else if(name){
+        headingValue += "?name=" + name;
+    }else if(year){
+        headingValue += "?year=" + year;
+    }
+    
+    heading.textContent=headingValue;
+}
+
+form.addEventListener("submit", submitFun);
+
+
